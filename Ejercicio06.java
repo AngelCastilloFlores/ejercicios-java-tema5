f885bc60381ac9fc82f377923f1fd4cf2ae2e12c
Ángel Castillo Flores <akmusic74@gmail.com>
2016-11-06 21:24:55 +0100

    Add files via upload

---
+++ Ejercicio06.java
@@ -0,0 +1,13 @@
+//Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás
+//utilizando un bucle while.
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		int i = 340;
+		while (i >= 160){
+			System.out.println(i-=20);
+		}
+
+	}
+
+}
